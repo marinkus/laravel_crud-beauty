1cfc3f2fa1c695723d9d3a4a727d2699209f76db
marinkus <marinkus@gmail.com>
2022-10-16 15:13:07 +0300

    responsive, block @medi max width 800px

--- v1/resources/views/welcome.blade.php
+++ v1/resources/views/welcome.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center block">
         <div class="col-4 justify-content-center hero-box">
             <h1>Hair stylist - MAC</h1>
             <p class="description">Ladies, when you look good - you feel good, when you feel good - you do good! Beauty is key to success!</p>
             <div class="buttons">
-                <a href="#">Gallery</a>s
-                <a href="#">About</a>
+                <a class="btn btn-secondary" href="#">Gallery</a>
+                <a class="btn btn-secondary" href="#">About</a>
             </div>
         </div>
         <div class="col-4 logo-box">
