4056d9527fb2bf9e9d2cb52ab33e8271b0bbe5c0
marinkus <marinkus@gmail.com>
2022-10-16 16:51:28 +0300

    few styling improvements

--- v1/resources/views/layouts/app.blade.php
+++ v1/resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 
 <body class="antialiased">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white navi">
+        <nav class="navbar navbar-expand-md navbar-light navi">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'MAC HairStylist') }}
