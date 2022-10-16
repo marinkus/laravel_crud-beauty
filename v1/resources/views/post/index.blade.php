b53fe81a27b21dc54e2776c78f2cd6753f5ece85
marinkus <marinkus@gmail.com>
2022-10-16 12:25:34 +0300

    index.blade base

--- v1/resources/views/post/index.blade.php
+++ v1/resources/views/post/index.blade.php
@@ -4,10 +4,28 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-8 col-lg-6 form-div">
-
+            <div class="col-8 col-lg-6 grid">
+                @forelse($posts as $post)
+                    <div class="col-3 form-div">
+                        <img src="#" alt="#">
+                        <h3 class="heading">{{ $post->title }}</h3>
+                        <p class="description">{{ $post->comment }}</p>
+                        <div class="buttons">
+                            <a href="{{ route('post_edit', $post) }}" type="button" class="btn btn-warning">Edit</a>
+                            <form action="{{ route('post_delete', $post) }}" method="post">
+                                @method('delete')
+                                @csrf
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                @empty
+                    NOTHING TO SHOW
+                @endforelse
 
 
             </div>
         </div>
     </div>
+
+@endsection
