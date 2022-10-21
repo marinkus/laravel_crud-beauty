86ba3520b3c3432103f693c4a905e12fba507572
marinkus <marinkus@gmail.com>
2022-10-21 09:34:50 +0300

    up

--- v1/resources/views/post/create.blade.php
+++ v1/resources/views/post/create.blade.php
@@ -4,26 +4,28 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 form-div mar-0">
-                <form action="{{route('post_store')}}" method="post" enctype="multipart/form-data">
+                <form action="{{ route('post_store') }}" method="post" enctype="multipart/form-data">
                     @csrf
-                <h3 class="heading">Create post</h3>
-                <div class="mb-3">
-                    <span class="input-group-text">Title</span>
-                    <input value="{{ old('title') }}" type="text" class="form-control" name="title">
-                </div>
-                <div class="mb-3">
-                    <span class="input-group-text">Price</span>
-                    <input value="{{ old('price') }}" type="number" step="0.01" class="form-control" name="price">
-                </div>
-                <div class="mb-3">
-                    <span class="input-group-text">Add photo</span>
-                    <input type="file" class="form-control" name="photo[]" multiple>
-                </div>
-                <div class="mb-3">
-                    <span class="input-group-text">Comment</span>
-                    <textarea class="form-control" name="comment">{{ old('comment') }}</textarea>
-                </div>
-                <button type="submit" class="btn btn-primary mt-4" @if (Auth::user()->role < 10) style="opacity: 0.3" disabled @endif>Publish</button>
+                    <h3 class="heading">Create post</h3>
+                    <div class="mb-3">
+                        <span class="input-group-text">Title</span>
+                        <input value="{{ old('title') }}" type="text" class="form-control" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <span class="input-group-text">Price</span>
+                        <input value="{{ old('price') }}" type="number" step="0.01" class="form-control"
+                            name="price">
+                    </div>
+                    <div class="mb-3">
+                        <span class="input-group-text">Add photo</span>
+                        <input type="file" class="form-control" name="photo[]" multiple>
+                    </div>
+                    <div class="mb-3">
+                        <span class="input-group-text">Comment</span>
+                        <textarea class="form-control" name="comment">{{ old('comment') }}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-4"
+                        @if (Auth::user()->role < 10) style="opacity: 0.3" disabled @endif>Publish</button>
                 </form>
             </div>
         </div>
